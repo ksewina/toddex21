e885f5a9f61e8d1157ab16e1dbab0b8037ed8c6c
ksewina <17175236+ksewina@users.noreply.github.com>
2024-06-13 18:28:38 +0200

    first commit

---
+++ ex15.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+
+	place := "World"
+	fmt.Println("Hello" ,place)
+	fmt.Println(puppy.Barks())
+	fmt.Println(puppy.Bark())
+
+}
